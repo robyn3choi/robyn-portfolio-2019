2cfa1add520428321eafdfa36dc73321086ca0ee
robyn <robyn3choi@gmail.com>
2019-04-30 11:04:12 -0700

    redid about section. working on music section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,55 +1,26 @@
 import Rellax from 'rellax';
+import * as utils from './utils';
 
 var rellax = new Rellax('.rellax');
 
-fixedSidebars();
 
-function fixedSidebars() {
-  const sidebars = document.getElementsByClassName('section__sidebar');
-  const sections = document.getElementsByClassName('section');
-  let sectionYs = [];
-  const startingY = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-  for (let section of sections) 
+// utils.workImageHover();
+
+
+
+const seeWorkDetails = () => 
+{
+  activeWork = document.getElementsByClassName('work__image_hover')[0].parentElement;
+  activeWork.classList.add('work_viewing-details');
+}
+
+window.onload = () => 
+{
+  utils.fixedSidebars();
+  utils.graphicHeadingAnim();
+  const workDetailsBtns = document.getElementsByClassName('work__details-button');
+  for (let btn of workDetailsBtns) 
   {
-    sectionYs.push(section.getBoundingClientRect().top + startingY);
+    btn.onclick = seeWorkDetails;
   }
-
-  const fixSidebarIfNeeded = () => {
-    const currentY = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-    // if we are before the 1st section
-    if (currentY < sectionYs[0] && sidebars[0].classList.contains('section__sidebar_fixed')) 
-    {
-      sidebars[0].classList.remove('section__sidebar_fixed');
-    }
-    else 
-    {
-      for (let i = 0; i < sectionYs.length; i++) 
-      {
-        if (i < sectionYs.length - 1) 
-        {
-          // if we are in a section
-          if (currentY > sectionYs[i]
-            && currentY < sectionYs[i + 1] - window.innerHeight
-            && !sidebars[i].classList.contains('section__sidebar_fixed')) 
-          {
-            sidebars[i].classList.add('section__sidebar_fixed');
-            sidebars[i].style.top = 0;
-            break;
-          }
-          // if we are in between sections
-          else if (currentY > sectionYs[i + 1] - window.innerHeight
-            && currentY < sectionYs[i + 1]) 
-          {
-            sidebars[i].classList.remove('section__sidebar_fixed');
-            sidebars[i+1].classList.remove('section__sidebar_fixed');
-            sidebars[i].style.top = sections[i].offsetHeight - window.innerHeight + 'px';
-            break;
-          }
-        }
-      }
-    }
-  };
-
-  window.addEventListener('scroll', fixSidebarIfNeeded);
-  fixSidebarIfNeeded();
 }
--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -1,7 +1,113 @@
-export const GetBacon = () => {
-  const body = fetch('https://baconipsum.com/api/?type=all-meat&paras=3').then(
-    res => res.json()
-  );
+export const fixedSidebars = () => 
+{
+  const sidebars = document.getElementsByClassName('section__sidebar');
+  const sections = document.getElementsByClassName('section');
+  let sectionYs = [];
 
-  return body;
-};
+  const resetSectionYs = () => {
+    const startingY = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    sectionYs = [];
+    for (let section of sections) 
+    {
+      sectionYs.push(section.getBoundingClientRect().top + startingY);
+    }
+  }
+
+  const fixSidebarIfNeeded = () => 
+  {
+    const currentY = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    // if we are before the 1st section
+    if (currentY < sectionYs[0] && sidebars[0].classList.contains('section__sidebar_fixed')) 
+    {
+      sidebars[0].classList.remove('section__sidebar_fixed');
+    }
+    else 
+    {
+      for (let i = 0; i < sectionYs.length; i++) 
+      {
+        if (i < sectionYs.length - 1) 
+        {
+          // if we are in a section
+          if (currentY > sectionYs[i]
+            && currentY < sectionYs[i + 1] - window.innerHeight
+            && !sidebars[i].classList.contains('section__sidebar_fixed')) 
+          {
+            console.log("in section " + i);
+            sidebars[i].classList.add('section__sidebar_fixed');
+            sidebars[i].style.top = 0;
+            break;
+          }
+          // if we are in between sections
+          else if (currentY > sectionYs[i + 1] - window.innerHeight
+            && currentY < sectionYs[i + 1]) 
+          {
+            console.log("between section " + i + "and " + (i+1));
+            sidebars[i].classList.remove('section__sidebar_fixed');
+            sidebars[i+1].classList.remove('section__sidebar_fixed');
+            sidebars[i].style.top = sections[i].offsetHeight - window.innerHeight + 'px';
+            break;
+          }
+        }
+      }
+    }
+  };
+
+  window.addEventListener('scroll', fixSidebarIfNeeded);
+  window.addEventListener('resize', () => {
+    resetSectionYs();
+    fixSidebarIfNeeded();
+  });
+
+  resetSectionYs();
+  fixSidebarIfNeeded();
+}
+
+
+// export const workImageHover = () => 
+// {
+//   const workImages = document.getElementsByClassName('work__image');
+//   for (img of workImages) 
+//   {
+//     img.addEventListener('mouseenter', e => e.target.classList.add('work__image_hover'));
+//     img.addEventListener('mouseleave', e => e.target.classList.remove('work__image_hover'));
+//   }
+// }
+
+
+export const graphicHeadingAnim = () => 
+{
+  const graphicHeadings = document.getElementsByClassName('graphic-heading');
+  let isScrolling = false;
+  let timeout;
+  // first, adjust width of graphic heading to match text
+  // for (let heading of graphicHeadings)
+  // {
+  //   const text = heading.parentElement.getElementsByClassName('graphic-heading__text')[0];
+  //   heading.parentElement.style.width = text.clientWidth.toString() + "px";
+  // }
+
+  const endAnim = () =>
+  {
+    for (let heading of graphicHeadings)
+    {
+      heading.classList.remove('graphic-heading_scrolling');
+    }
+    isScrolling = false;
+  }
+
+  const startAnim = () => 
+  { 
+    clearTimeout(timeout);
+    timeout = setTimeout(endAnim, 20);
+    if (!isScrolling) 
+    {
+      for (let heading of graphicHeadings)
+      {
+        heading.classList.add('graphic-heading_scrolling');
+      }
+      isScrolling = true;
+    }
+  }
+
+  window.addEventListener('scroll', startAnim);
+}
