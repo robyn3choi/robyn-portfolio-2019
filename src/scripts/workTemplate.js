a95217d5980f0e98e0b7bf172cf03359fb55f7f8
robyn <robyn3choi@gmail.com>
2019-05-02 17:36:45 -0700

    populated work data

--- src/scripts/workTemplate.js
+++ src/scripts/workTemplate.js
@@ -16,34 +16,61 @@ const data = {
         `Google Maps Places Search Box`,
         `stores user’s location in browser's local storage`,
         `CSS animations`,
-        `CSS animations`
+        `responsive design`,
       ]
     },
 
-    { // weather app
-      "image": "howsthesky.png",
+    { // stu website
+      "image": "stu.png",
       "summaryParagraphs": [
-      `A sleek, minimal weather web app for when you just want the weather and nothing else. 
-      It uses the Dark Sky API for weather data and the OpenCage API for geocoding.`
+      `A unique yet simple portfolio website for music producer Stuart Brookes.`
       ,
-      `The front end is built with React and deployed with Netlify, and the back end is built with
-      Node/Express and hosted on a DigitalOcean droplet with Dokku.`
+      `The front end is built with React and deployed with Netlify. 
+      The site uses headless CMS Prismic.io to allow the client to easily edit information on the website without needing to look at code.
+      `
       ],
       "features": [
-        `location detection`,
-        `Google Maps Places Search Box`,
-        `stores user’s location in browser's local storage`,
-        `CSS animations`,
-        `CSS animations`
+        `original art and animations`,
+        `music section displays songs in a filterable grid`,
+        `easily editable by the client using Prismic.io`,
+        `responsive design`
       ]
     },
 
+    { // fury website
+      "image": "fury.png",
+      "summaryParagraphs": [
+      `A website for music duo WE ARE FURY that displays their top tracks, tour dates, and merchandise for sale.`
+      ,
+      `The was built using jQuery and is hosted on HostGator (planning to migrate to GitHub Pages).`
+      ],
+      "features": [
+        `Shopify integration`,
+        `image parallax effect`,
+        `responsive design`
+      ]
+    },
+
+    { // old portfolio
+      "image": "old-portfolio.png",
+      "summaryParagraphs": [
+      `My previous portfolio website, geared towards game development. As you scroll down the page, a pixel art avatar of myself walks down
+      and gives you a tour of Vancouver. The site won 1st place in the Judges' Choice category and runner-up in the People's Choice category at 
+      the University of British Columbia's e-Portfolio Competition 2017.`
+      ,
+      `The was built using jQuery and is hosted on GitHub Pages.`
+      ],
+      "features": [
+        `original art and animations`,
+        `responsive design`
+      ]
+    }
   ]
 }
 
 const injectWorks = () => 
 {
-  let html;
+  let html = '';
   for (let work of data.works) 
   {
     let summaryParagraphsHtml = '';
