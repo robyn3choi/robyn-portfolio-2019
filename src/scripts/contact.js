28150d2674dd4f8293f43e23e6f1a356ba3202b0
robyn <robyn3choi@gmail.com>
2019-05-29 12:37:32 -0700

    made wood grain thinner, changed header anim font, finished contact section

--- src/scripts/contact.js
+++ src/scripts/contact.js
@@ -1,22 +1,22 @@
 import {isElementInViewport, playTypingAnimation} from './utils';
 
 let contactEmailAnim;
-let hasPlayedContactAnim = false;
+const hasPlayedContactAnim = false;
 
 export const initContactAnim = () => {
-  contactEmailAnim = document.getElementById('contact__email');
+  // contactEmailAnim = document.getElementById('contact__email');
   // playContactAnimIfNeeded();
 };
 
 export const playContactAnimIfNeeded = () => {
-  if (!hasPlayedContactAnim && isElementInViewport(contactEmailAnim)) {
-    setTimeout(
-        () =>
-          playTypingAnimation(document.getElementById('contact__email'), 'robyn3choi@gmail.com', 50),
-        1200
-    );
-    hasPlayedContactAnim = true;
-    return true;
-  }
+  // if (!hasPlayedContactAnim && isElementInViewport(contactEmailAnim)) {
+  //   setTimeout(
+  //       () =>
+  //         playTypingAnimation(document.getElementById('contact__email'), 'robyn3choi@gmail.com', 50),
+  //       1200
+  //   );
+  //   hasPlayedContactAnim = true;
+  //   return true;
+  // }
   return false;
 };
