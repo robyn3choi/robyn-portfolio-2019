2b6eef171b75970abdbe52b6ff622e3258d8578b
robyn <robyn3choi@gmail.com>
2019-05-30 11:06:50 -0700

    pause about anim when not in view, put wood grain on works stacked sidebar img

--- src/scripts/about.js
+++ src/scripts/about.js
@@ -25,38 +25,53 @@ export const initAboutAnim = () => {
   });
 };
 
+let isSidebarAnimPlaying = false;
 export const playAboutAnimIfNeeded = () => {
-  if (!hasStartedSidebarAnim && isStackedSections() && isElementInViewport(aboutSidebar)) {
-    aboutAnim.playSegments([[10, 27], [28, 101]], true);
-    hasStartedSidebarAnim = true;
+  if (hasStartedSidebarAnim) {
+    // if sidebar is offsreen
+    const aboutSidebarBottom = aboutSidebar.getBoundingClientRect().bottom;
+    if (isSidebarAnimPlaying && aboutSidebarBottom <= 0) {
+      console.log('pause');
+      aboutAnim.pause();
+      isSidebarAnimPlaying = false;
+    }
+    else if (!isSidebarAnimPlaying && aboutSidebarBottom > 0) {
+      console.log('resume');
+      aboutAnim.playSegments([[28, 101]], true);
+      isSidebarAnimPlaying = true;
+    }
   }
-  if (!hasStartedTypingAnim && isElementInViewport(aboutTyped)) {
-    hasStartedTypingAnim = true;
-    if (!hasStartedSidebarAnim) {
+  else {
+    if (!hasStartedSidebarAnim && isStackedSections() && isElementInViewport(aboutSidebar)) {
       aboutAnim.playSegments([[10, 27], [28, 101]], true);
       hasStartedSidebarAnim = true;
     }
-    setTimeout(() => {
-      playTypingAnimation(
-          document.getElementById('about__typed_1'),
-          `Hey there, I'm Robyn Choi.`,
-          35
-      )
-          .then(() =>
-            playTypingAnimation(
-                document.getElementById('about__typed_2'),
-                `I'm a full-stack web developer based in Vancouver, Canada.`,
-                15
+    if (!hasStartedTypingAnim && isElementInViewport(aboutTyped)) {
+      hasStartedTypingAnim = true;
+      if (!hasStartedSidebarAnim) {
+        aboutAnim.playSegments([[10, 27], [28, 101]], true);
+        hasStartedSidebarAnim = true;
+      }
+      setTimeout(() => {
+        playTypingAnimation(
+            document.getElementById('about__typed_1'),
+            `Hey there, I'm Robyn Choi.`,
+            35
+        )
+            .then(() =>
+              playTypingAnimation(
+                  document.getElementById('about__typed_2'),
+                  `I'm a full-stack web developer based in Vancouver, Canada.`,
+                  15
+              )
             )
-          )
-          .then(() => {
-            hasFinishedTypingAnim = true;
-            playEnterAnimsIfNeeded();
-          });
-    }, 1000);
-    return true;
+            .then(() => {
+              hasFinishedTypingAnim = true;
+              playEnterAnimsIfNeeded();
+            });
+      }, 1000);
+    }
   }
-  return false;
 };
 
 export const hasPlayedAboutTypingAnim = () => {
